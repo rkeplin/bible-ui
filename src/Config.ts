f7ec75fec4866d123d937e44fd6d774eaa695a47
Rob Keplin <rkeplin@gmail.com>
2021-04-18 12:48:35 -0400

    pulls api url vars from env

--- src/Config.ts
+++ src/Config.ts
@@ -1,10 +1,9 @@
-// TODO: Pull these from env
 const API = 'http://bible-go-api.rkeplin.local/v1';
 const USER_API = 'http://bible-php-api.rkeplin.local/v1';
 
 class Config {
-    public static readonly API = API;
-    public static readonly USER_API = USER_API;
+    public static readonly API = process.env.REACT_APP_API_URL || API;
+    public static readonly USER_API = process.env.REACT_APP_USER_API_URL || USER_API;
 }
 
 export default Config;
